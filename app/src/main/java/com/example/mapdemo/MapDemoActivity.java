fb8dfb64199872117eaa4efff6316befd9015fce
Cierra Ouellette <cierraouellette@fb.com>
2022-06-15 11:07:19 -0700

    Added Pin Dropping Animation and Custom info window

--- app/src/main/java/com/example/mapdemo/MapDemoActivity.java
+++ app/src/main/java/com/example/mapdemo/MapDemoActivity.java
@@ -7,11 +7,13 @@ import android.content.DialogInterface;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Looper;
+import android.os.SystemClock;
 import android.text.Layout;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.BounceInterpolator;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -85,12 +87,45 @@ public class MapDemoActivity extends AppCompatActivity implements GoogleMap.OnMa
                 @Override
                 public void onMapReady(GoogleMap map) {
                     loadMap(map);
+                    map.setInfoWindowAdapter(new CustomWindowAdapter(getLayoutInflater()));
                 }
             });
         } else {
             Toast.makeText(this, "Error - Map Fragment was null!!", Toast.LENGTH_SHORT).show();
         }
 
+    }
+
+    private void dropPinEffect(final Marker marker) {
+        // Handler allows us to repeat a code block after a specified delay
+        final android.os.Handler handler = new android.os.Handler();
+        final long start = SystemClock.uptimeMillis();
+        final long duration = 1500;
+
+        // Use the bounce interpolator
+        final android.view.animation.Interpolator interpolator =
+                new BounceInterpolator();
+
+        // Animate marker with a bounce updating its position every 15ms
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                long elapsed = SystemClock.uptimeMillis() - start;
+                // Calculate t for bounce based on elapsed time
+                float t = Math.max(
+                        1 - interpolator.getInterpolation((float) elapsed
+                                / duration), 0);
+                // Set the anchor
+                marker.setAnchor(0.5f, 1.0f + 14 * t);
+
+                if (t > 0.0) {
+                    // Post this event again 15ms from now.
+                    handler.postDelayed(this, 15);
+                } else { // done elapsing, show window
+                    marker.showInfoWindow();
+                }
+            }
+        });
     }
 
     protected void loadMap(GoogleMap googleMap) {
@@ -268,6 +303,7 @@ public class MapDemoActivity extends AppCompatActivity implements GoogleMap.OnMa
                         .title(title)
                         .snippet(snippet)
                         .icon(defaultMarker));
+                dropPinEffect(marker);
             }
         });
         alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel",
